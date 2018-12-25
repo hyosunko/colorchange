3c9971adc1f5a26acfb0770889dd742e4173bf9e
hyosunko <hyosunko@gmail.com>
2018-12-24 18:03:16 -0800

    Updates

---
+++ precache-manifest.c6d5e0b98c6544c674c28149f5c1eb94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f343d1b4a8e9396b646",
+    "url": "/colorchange/static/js/runtime~main.7f343d1b.js"
+  },
+  {
+    "revision": "0ff18c1b3f05333fb064",
+    "url": "/colorchange/static/js/main.0ff18c1b.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/colorchange/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "0ff18c1b3f05333fb064",
+    "url": "/colorchange/static/css/main.b23c58a5.chunk.css"
+  },
+  {
+    "revision": "a5301110259efd4365076c146f0aaec0",
+    "url": "/colorchange/index.html"
+  }
+];
